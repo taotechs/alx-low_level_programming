3f01d9499897f0d948dd655e14c7c29460ad08ed
taotechs <taofiqakintunde@gmail.com>
2022-12-08 10:36:25 -0800

    with proper grammar, but the outcome is a piece of art

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
+/* with proper grammar, but the outcome is a piece of art*/
 int main(void)
 {
- printf("with proper grammar, but the outcome is a piece of art,\n");
+printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
 }
