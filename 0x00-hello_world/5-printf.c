938568eb0fea7be60fa6b04a52d8d191be846d7d
taotechs <taofiqakintunde@gmail.com>
2022-12-08 10:45:19 -0800

    with proper grammar, but the outcome is a piece of art

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,6 @@
 #include <stdio.h>
-/**
- *with proper grammar, but the outcome is a piece of art
- *Return: 0 if exited properly, non-zero otherwise
- */
-int main(void)
+/*with proper grammar, but the outcome is a piece of art */
+int main(void) /* manin driver */				  
 {
 printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
