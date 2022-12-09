86db8c849b1a327558a6e65ddce6aff23990bd0b
taotechs <taofiqakintunde@gmail.com>
2022-12-09 08:55:14 -0800

    advance

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - Entry point
+ * Description: prints two digits combination with some conditions
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int num, i;
+	for (num = '0'; num <= '9'; num++)
+	{
+		for (i = '0'; i <= '9'; i++)
+		{
+			if (num < i)
+			{
+				putchar(num);
+				putchar(i);
+				if (num != '8' || (num == '8' && i != '9'))
+				{
+					putchar(',');
+					putchar(' ');
+	}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
