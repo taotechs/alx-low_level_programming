92f36d36115ec522d26e78080119a040fd19bbb5
taotechs <taofiqakintunde@gmail.com>
2022-12-09 08:03:30 -0800

    numbers with comma and spaces

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - drivers code by taotechs
+ * Description: This program prints all alphabets with commas and spaces
+ * Return: it returns 0 at the end of the program
+*/
+int main(void)
+{
+char test;
+for (test = 'a'; test <= 'z'; test++)
+{
+putchar(test);
+putchhar(',');
+putchar(' ');
+}
+putchar('\n');
+return (0);
+}
