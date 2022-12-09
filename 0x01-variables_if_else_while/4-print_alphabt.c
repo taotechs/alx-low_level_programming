6e85e2f695fdd5a79666fc04ea4af7ee2a0d2781
taotechs <taofiqakintunde@gmail.com>
2022-12-09 04:05:09 -0800

    q and e omitted in lower case alphabets

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - drivers code by taotechs
+ * Description: This program prints all alphabets
+ * Return: it returns 0 at the end of the program
+*/
+int main(void)
+{
+char test;
+for (test = 'a'; test <= 'z'; test++)
+{
+	if (test == 'e' || test == 'q')
+	continue;
+putchar(test);
+}
+putchar('\n');
+return (0);
+}
