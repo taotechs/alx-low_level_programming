4eed1049b1ae326169e4cef262bf9c7bbf92cd3a
taotechs <taofiqakintunde@gmail.com>
2022-12-09 03:27:36 -0800

    prints details about last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -2,16 +2,17 @@
 #include <stdio.h>
 #include <time.h>
 /**
- * This prints the last digit of a number and output some stuffs
- * Below is the main driver code
+ * main - Drivers code
+ * Description: This prints the last digit of a number and output some stuffs
+ * Return: 0 for success
  */
 int main(void)
 {
-int n,x;
+int n, x;
 srand(time(0));
 n = rand() - RAND_MAX / 2;
-x= n%10;
-if(x>5)
+x = n % 10;
+if (x > 5)
 {
 printf("Last digit of %d is %d and is greater than 5\n", n, x);
 }
@@ -19,7 +20,7 @@ else if (x == 0)
 {
 	printf("Last digit of %d is %d and is 0\n", n, x);
 }
-else if (x < 6 && x!=0)
+else if (x < 6 && x != 0)
 {
 	printf("Last digit of %d is %d and is not 0\n", n, x);
 }
