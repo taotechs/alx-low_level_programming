d79e12fcb3321a98f6c84c6ea5eacc9df6cc78ea
taotechs <taofiqakintunde@gmail.com>
2022-12-09 03:50:34 -0800

    alphabets in lower case and upper case

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -7,15 +7,15 @@
 */
 int main()
 {
-	char test;
-
-	        for (test= 'a'; test< 'z'; test++){
-			        putchar(test);
-		}
-		for (test= 'A'; test< 'Z'; test++){
-				putchar(test);
-		}
-		putchar('\n');
-			return 0;
-
+char test;
+for (test = 'a'; test <= 'z'; test++)
+{
+putchar(test);
+}
+for (test = 'A'; test <= 'Z'; test++)
+{
+putchar(test);
+}
+putchar('\n');
+return 0;
 }
