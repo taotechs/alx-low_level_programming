f5b2499bd8e7081245eae6fb718d5d3558232e31
taotechs <taofiqakintunde@gmail.com>
2022-12-09 03:45:02 -0800

    alphabets in lower case and upper case

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,28 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- * This program prints all alphabetsb oth upper and lower case
- * it returns 0 at the end of the program
+ * main - Drivers code by taotechs
+ * Description: This program prints all alphabetsb oth upper and lower case
+ * Return: it returns 0 at the end of the program
 */
 int main()
 {
-
-	    char test;
+	char test;
 
 	        for (test= 'a'; test< 'z'; test++){
-
-			        putchar(test);
-
-				    }
-
-		     for (test= 'A'; test< 'Z'; test++){
-
-			             putchar(test);
-
-				         }
-
-		     	putchar('\n');    
-
-			    return 0;
+			        putchar(test)
+		}
+		for (test= 'A'; test< 'Z'; test++){
+				putchar(test);
+		}
+		putchar('\n');
+			return 0;
 
 }
