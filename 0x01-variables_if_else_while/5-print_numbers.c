747af6d3e851d49db253143d2fef390992eb95ad
taotechs <taofiqakintunde@gmail.com>
2022-12-09 04:12:04 -0800

    print 0 to 9

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - driver code
+ * Description: generate numbers from 0 to 9
+ * Return: 0 for success
+*/
+int main(void)
+{
+int n;
+for(n = 0; n < 10; n++){
+	printf("%d", n);
+}
+printf("\n");
+return (0);
+}
