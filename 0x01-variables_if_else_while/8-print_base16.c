5ca579c4dcc449232b29165cb424da8114e04d86
taotechs <taofiqakintunde@gmail.com>
2022-12-09 06:39:35 -0800

    base 16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Drivers code by taotechs
+ * Descripton: This program prints all alphabets
+ * Return: it returns 0 at the end of the program
+*/
+int main()
+{
+char test;
+int num;
+for (num = 0; num < 10; num++)
+{
+putchar(num + '0');
+}
+for (test = 'a'; test <= 'f'; test++)
+{
+putchar(test);
+}   
+putchar("\n");  
+return (0);
+}
