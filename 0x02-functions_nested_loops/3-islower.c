58b83f336526fd4245a5c084880d8b2a18df1e2b
taotechs <taofiqakintunde@gmail.com>
2022-12-13 07:05:02 -0800

    _islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * Islower - Test if a letter is lowercase or not
+ * Description: code checker
+ * Return: islower valui
+*/
+int _islower(char c)
+{
+int i = 'a';
+
+for (i = 'a'; i <= 'z'; i++)
+
+{
+
+	/* refer int c*/
+
+	if (c == i)
+	{
+	return (1);
+	}
+}
+return (0);
+}
