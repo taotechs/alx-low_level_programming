9738eb2713ede8bc71a7e1153783fc182e4ca0f0
taotechs <taofiqakintunde@gmail.com>
2022-12-13 05:47:44 -0800

    print _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 /**
- * main  Drivers code for program by taotechs
+ * main - Drivers code for program by taotechs
  * Description: This program prints _putchar
  * Return: Always 0 (success)
 */
-int main(void){
+int main(void)
+{
 char msg[] = "_putchar\n";
 int i;
 for (i = 0; i < 13; i++)
