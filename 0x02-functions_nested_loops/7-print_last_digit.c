afa938eb4421a5ac4637d961fec181973d8f1334
taotechs <taofiqakintunde@gmail.com>
2022-12-13 07:50:16 -0800

    print last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,13 @@
+int print_last_digit(int num)
+/**
+ * print_last_digit - driver code
+ * Description - prints last digit of a number
+ * @num: is the parameter
+ * Return: last digit
+*/
+{
+	
+	int last;
+last = num%10;
+return (last);
+}
