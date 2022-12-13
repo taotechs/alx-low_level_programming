0c76fb383d33a14ee0b102a0bcd78345d6e519e6
taotechs <taofiqakintunde@gmail.com>
2022-12-13 07:08:18 -0800

    _islower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,5 +1,9 @@
 #include "main.h"
 /**
+ * _islower - Entry point
+ * Description: checks is a character is lowercase
+ * @c: the integer value it receives
+ * Return: 1 if true. 0 if false.
  * Islower - Test if a letter is lowercase or not
  * Description: code checker
  * Return: islower valui
