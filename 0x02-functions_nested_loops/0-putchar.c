006a984e7573daf8aa79b83d66b3f2a9a1a074b5
taotechs <taofiqakintunde@gmail.com>
2022-12-13 03:54:05 -0800

    first task

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main  Drivers code for program by taotechs
+ * Description: This program prints _putchar
+ * Return: Always 0 (success)
+*/
+int main (void){
+char msg[] = "_putchar\n";
+int i;
+for (i = 0; i < 13; i++)
+{
+	if (msg[i] == '\n'){
+	putchar('\n');
+	break;
+	}
+	else
+	{
+	putchar(msg[i]);
+	}
+}
+return (0);
+}
---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print alphabets in lowercase
+ * return void
+*/
+void print_alphabet(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		putchar(i);
+	}
+		putchar('\n');
+}
+/**
+ * main - drivers code
+ * Description: call the print_alphabets function
+ * Return: 0 for success
+*/
+int main(void)
+{
+print_alphabet();
+return (0);
+}
