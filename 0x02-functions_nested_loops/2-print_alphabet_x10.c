82f3269aabaaf0a6fcc2b0e8d99ef3445b23dc3b
taotechs <taofiqakintunde@gmail.com>
2022-12-13 06:27:08 -0800

    prints alphabets 10 times

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,11 +1,12 @@
-#include <stdio.h>
 #include "main.h"
 /**
+ * function to print alphabets 10 times
  * print alphabets in lowercase x10
  * return void
 */
 void print_alphabet_x10(void)
 {
+	int i;
 	char i;
 
 	for (i = 'a'; i <= 'z'; i++)
@@ -13,18 +14,8 @@ void print_alphabet_x10(void)
 		putchar(i);
 	}
 		putchar('\n');
-}
-/**
- * main - drivers code
- * Description: call the print_alphabets function
- * Return: 0 for success
-*/
-int main(void)
-{
-	int i;
 	for (i = 1; i <= 10; i++)
 	{
 	print_alphabet_x10();
 	}
-return (0);
 }
