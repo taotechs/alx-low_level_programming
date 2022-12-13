d3e126a61b613744554aabf1c17b5e345e8cfe33
taotechs <taofiqakintunde@gmail.com>
2022-12-13 07:33:46 -0800

    test for alphabet

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,11 +1,10 @@
-nclude "main.h"
-
+#include "main.h"
 /**
-   * _isalpha - Entry point
-    * @c: the integer value it receives
-     * Description: checks if alphabet
-      * Return: 1 if true. 0 if false
-       */
+ * _isalpha - Entry point
+ * @c: the integer value it receives
+ * Description: checks if alphabet
+ * Return: 1 if true. 0 if false
+ */
 
 int _isalpha(int c)
 
