452b1648b429d63a464cc26536facee32381ac90
taotechs <taofiqakintunde@gmail.com>
2022-12-15 08:23:31 -0800

    one to nine except 4&2

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,21 +1,23 @@
 #include "main.h"
 
 /**
- * print_numbers - print  0 - 9 except 2&4
+ * print_most_numbers - prints 0 - 9 except 2, 3
+ * Description: prints
  * Return: void
  */
 
-void print_numbers(void)
+void print_most_numbers(void)
 
 {
-int a;
 
-for (a = 0; a <= 9 a++)
+int a = 0;
+while (a <= 9)
 {
-	if (a != 2 && a != 4)
-	{
-          _putchar(a + '0');
-	}
+if (a != 2 && a != 4)
+{
+_putchar(a + '0');
+}
+a++;
 }
 _putchar('\n');
 
