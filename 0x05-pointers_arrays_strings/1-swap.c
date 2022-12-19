1d9f6242f7f9b24cd8ba3bd8e969ffa3d8d687eb
taotechs <taofiqakintunde@gmail.com>
2022-12-19 07:55:07 -0800

    swaps pointers value

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,13 @@
+/**
+ * 1-swap.c - swap values of pointers
+ * @a: first pointer
+ * @b: second pointer
+ * Return: Nothing
+*/
+void swap_int(int *a, int *b)
+{
+	int tmp = *a;
+	*a = *b;
+	*b = tmp;
+}
+
